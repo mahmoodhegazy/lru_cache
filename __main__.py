af4f0aef2f5188489d636d30128ba0a891ce36b7
Mahmood Hegazy <mahmoodhegazy@Mahmoods-MacBook-Pro-3.local>
2021-01-13 15:10:35 -0500

    Remove redundant main test file

--- __main__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-import unittest
-
-
-if __name__ == "__main__":
-    suite = unittest.TestLoader().discover('test', pattern="test_*.py")
-    result = unittest.TextTestRunner(verbosity=3).run(suite)
